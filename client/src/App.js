e4b06b99b6f987e47753e9c622600b533ece8bf0
Suyash Agrahari <suyashagrahari73@gmail.com>
2023-10-16 15:33:50 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import Home from "./pages/home/Home";
+import TopBar from "./components/topbar/TopBar";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Setting";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
+
+function App() {
+  const { user } = useContext(Context);
+  return (
+    <>
+    <Router>
+    <TopBar />
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home /> : <Register />}</Route>
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
